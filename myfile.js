73155205e1ce7b38cd84a5253f310774430cb02f
a2sandoval <alejandrosandoval@MacBook-Pro-7.local>
2018-07-21 19:43:54 -0700

    Final

--- myfile.js
+++ myfile.js
@@ -1,23 +1,23 @@
 
+  // Initialize Firebase
   var config = {
-    apiKey: "AIzaSyBBz0ZQ-pm6WRjpRk0yp3hwdIzwSdG1eU4",
-    authDomain: "first-project-84717.firebaseapp.com",
-    databaseURL: "https://first-project-84717.firebaseio.com",
-    projectId: "first-project-84717",
-    storageBucket: "first-project-84717.appspot.com",
-    messagingSenderId: "1024553920268"
+    apiKey: "AIzaSyCUtzbRBmJfOwu33vE_5PGbVK7RWrKlLTY",
+    authDomain: "homework-train.firebaseapp.com",
+    databaseURL: "https://homework-train.firebaseio.com",
+    projectId: "homework-train",
+    storageBucket: "homework-train.appspot.com",
+    messagingSenderId: "610009520786"
   };
-
   firebase.initializeApp(config);
 
   var database = firebase.database();
 
   $("#form-submit").on("click", function() {
     database.ref().push({
-        name:$("#employee-name").val().trim(),
-        title: $("#employee-title").val().trim(),
-        start: $("#start-date").val().trim(),
-        monthlyRate: $("#monthly-rate").val().trim()
+        name:$("#train-name").val().trim(),
+        title: $("#train-destination").val().trim(),
+        start: $("#arrival-time").val().trim(),
+        frequency: $("#frequency").val().trim(),
 
       });
     });
@@ -27,28 +27,42 @@
         var empName = snapshot.val().name;
         var empTitle = snapshot.val().title;
         var empStart= snapshot.val().start;
-        var emptMonthlyRate = snapshot.val().monthlyRate;
-        var months = moment().diff(empStart, "months");
-
-        var parsed1 = parseInt(emptMonthlyRate,10);
-        var parsed2 = parseInt(months,10);
-        var billed = parsed1 * parsed2;
+        var emptfrequency = snapshot.val().frequency;
+        var convertTimeStart = moment(empStart, "hh:mm").subtract(1, "years");
+        var localTime = moment();
+        var timeDiff = moment().diff(moment(convertTimeStart), "minutes");
+        var timeRemain = timeDiff % emptfrequency;
+        var minutesTillTrain = emptfrequency - timeRemain;
+        var nextTrain = moment().add(minutesTillTrain, "minutes");
+        var nextTrainFormatted = moment(nextTrain).format("HH:mm");
         
-        console.log(billed);
-        console.log(months);
-        console.log(emptMonthlyRate);
+        console.log(localTime);
+        console.log(timeDiff);
+        console.log(timeRemain);
+        console.log(emptfrequency);
+        console.log(nextTrain);
+        console.log(minutesTillTrain);
+        console.log(nextTrainFormatted);
 
         var newRow = $("<tr>").append(
              $("<td>").text(empName), 
              $("<td>").text(empTitle),
              $("<td>").text(empStart),
-             $("<td>").text(emptMonthlyRate),
-             $("<td>").text(months),
-             $("<td>").text(billed)
+             $("<td>").text(emptfrequency),
+             $("<td>").text(nextTrainFormatted)
             );
+    
 
     $(".table").append(newRow);
 
 }, function(errorObject) {
     console.log("The read failed: " + errorObject.code);
 });
+
+function init() {
+  var input = document.getElementById('train-destination');
+  var autocomplete = new google.maps.places.Autocomplete(input);
+}
+
+
+google.maps.event.addDomListener(window, 'load', init); 
